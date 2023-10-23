4734a324a0621751f095b0c66d7294705308d34e
Eitan Liu <eitanliu@gmail.com>
2023-10-23 17:07:02 +0800

    release 0.0.1

--- lib/src/framework/mvvm/viewmodel/base_viewmodel.dart
+++ lib/src/framework/mvvm/viewmodel/base_viewmodel.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:flutter/foundation.dart';
-import 'package:uuid/uuid.dart';
 
 import '../../router/navigator_properties.dart';
 import '../properties/dialog_properties.dart';
@@ -12,14 +11,8 @@ import '../properties/dialog_properties.dart';
 abstract class BaseViewModel extends ChangeNotifier
     with DialogProperties
     implements NavigatorProperties {
-  String basicPageViewModel = "";
 
-  String pageRandom() {
-    if (basicPageViewModel.isEmpty) {
-      basicPageViewModel = const Uuid().v4().replaceAll("-", "");
-      basicPageViewModel =
-          "$basicPageViewModel--${DateTime.now().microsecondsSinceEpoch}--${Random().nextDouble()}";
-    }
-    return "$runtimeType-$basicPageViewModel";
+  String pageIdentity() {
+    return "$runtimeType-${identityHashCode(this)}";
   }
 }
